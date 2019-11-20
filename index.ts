5179be1038ff7e74ae25065bdb743ad81225abc0
ptouchton <p_touchton@yahoo.com>
2019-11-20 14:36:12 -0800

    now I can sleep

--- index.ts
+++ index.ts
@@ -1,6 +1,37 @@
 // Import stylesheets
 import './style.css';
 
+function isBalanced(input: string){
+
+    let stack = [];
+    let openingBrackets = ['[', '{', '(']
+    let closingBrackets = [']', '}', ')']
+
+    for(let i=0; i < input.length; i++) {
+      let c = input[i];
+
+      if (openingBrackets.indexOf(c) > -1){
+         stack.push(c);
+      }else{
+        let val = stack.pop();
+        let matchingBrackets = openingBrackets[closingBrackets.indexOf(c)];
+        
+        if (val != matchingBrackets){
+            return false;
+        }
+
+      }
+
+    }
+
+    return true;
+
+}
+
+
+let inputString = '({[]}}';
+let response = isBalanced(inputString);
+
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+appDiv.innerHTML = `<h1>Input: ${inputString} result ${response}</h1>`;
